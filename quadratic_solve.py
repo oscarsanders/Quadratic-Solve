b826dc6d86c98e73d1cbd19396dcd551b665aabc
oscarsanders <49376381+oscarsanders@users.noreply.github.com>
2023-04-08 18:51:08 -0700

    Add files via upload
    function criteria & handle errors

--- quadratic_solve.py
+++ quadratic_solve.py
@@ -1,28 +1,45 @@
-# solicitamos al usuario los valores
-# de los coeficientes a, b y c.
-print("==================================================")
-initial = True
-while initial:
-    print("Escribe C para terminar y salir")
-    print("Este programa resuelve una ecuación")
-    print("cuadratica de la forma ax² + bx + c = 0")
-    a = float(input("Escribe el valor de a: "))
-    b = float(input("Escribe el valor de b: "))
-    c = float(input("Escribe el valor de c: "))
+# this function check input values
+def criteria():
+  global a,b,c
+  while True:
+    try:
+      # valor = input("Ingresa un número: ")
+      a = float(input("Write the value of a: "))
+      b = float(input("Write the value of b: "))
+      c = float(input("Write the value of c: "))
 
-    # discriminante b²-4ac
-    dis = b**2 - 4*a*c
+      # discriminant b²-4ac
+      dis = b**2 - 4*a*c
 
-    if dis > 0:
+      if dis > 0:
         print("Dos soluciones reales")
-    elif dis == 0:
+      elif dis == 0:
         print("Soluciones repetidas")
-    else:
+      elif dis < 0:
         print("Soluciones complejas")
+
+      # x1 = (-b + dis**0.5)/2*a # solucion 1
+      # x2 = (-b - dis**0.5)/2*a # solucion 2
+      # print solution on screen
+      # print(f"Las soluciones son x =  {x1:.2f}; x = {x2:.2f}")
+
+      return dis
+    except ValueError:
+      # if valor.upper() == 'F':
+      #   break
+      print("Error: El valor ingresado no es un número válido. Intenta de nuevo.")
+      
+# main program
+print("==================================================")
+while True:
+    print("This program solve a quadratic")
+    print("equation of the form ax² + bx + c = 0")
+    
+    dis = criteria()
 
     x1 = (-b + dis**0.5)/2*a # solucion 1
     x2 = (-b - dis**0.5)/2*a # solucion 2
-    # se imprimen en pantalla los resultados
+    # print solution on screen
     print(f"Las soluciones son x =  {x1:.2f}; x = {x2:.2f}")
     print("==================================================")
     finish = input("Presiona F para terminar ó \
